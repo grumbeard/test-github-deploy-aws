310c82eb99a359296417ea65dc566019a88f54f8
Kaiying Fu <kaiying.fu@gmail.com>
2021-10-26 10:44:00 +0800

    Setup Base Code

---
+++ test-container/main.tf
@@ -0,0 +1,25 @@
+resource "aws_ecr_repository" "api-service-registry" {
+  name = "devtools-test-1"
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
+
+# Creates Task Definition with placeholder image
+# Image will be inserted in Task Definition by CI
+resource "aws_ecs_task_definition" "api-service" {
+  family = "api-service-test-1"
+  container_definitions = jsonencode([{
+    "name" : "api-service-container-test-1",
+    "image" : "scratch",
+    "memory" : 512,
+    "portMappings" : [
+      {
+        "containerPort" : 22,
+        "hostPort" : 22
+        "protocol" : "tcp"
+      }
+    ]
+  }])
+  requires_compatibilities = ["EC2"]
+}
